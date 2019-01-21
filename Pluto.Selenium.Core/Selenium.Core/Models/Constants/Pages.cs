f6f361372ab05c6e396dbd4d6c0177826395cc07
Pluto Solutions <john@pluto.solutions>
2019-01-22 00:57:19 +0700

    Initial Selenium .NET Development by Pluto Solutions

---
+++ Pluto.Selenium.Core/Selenium.Core/Models/Constants/Pages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoSeleniumCore.Models
+{
+    public class Pages
+    {
+        public static string work_and_study { get { return ""; } }
+        public static string lp_administrative_assistant { get { return ""; } }
+        public static string lp_android_developer { get { return ""; } }
+        public static string lp_art_director { get { return ""; } }
+        public static string lp_customer_care_representative_english { get { return ""; } }
+        public static string lp_customer_care_representative_french { get { return ""; } }
+        public static string lp_customer_care_representative_german { get { return ""; } }
+        public static string lp_customer_care_representative_italian { get { return ""; } }
+        public static string lp_customer_care_representative_spanish { get { return ""; } }
+        public static string lp_customer_care_representative_swiss { get { return ""; } }
+        public static string lp_customer_care_representative_thai { get { return ""; } }
+        public static string lp_it_manager { get { return ""; } }
+        public static string lp_java_developer { get { return ""; } }
+        public static string lp_office_assistant { get { return ""; } }
+        public static string lp_php_developer { get { return ""; } }
+        public static string lp_sales_agent_english { get { return ""; } }
+        public static string lp_sales_agent_french { get { return ""; } }
+        public static string lp_sales_agent_german { get { return ""; } }
+        public static string lp_sales_agent_italian { get { return ""; } }
+        public static string lp_sales_agent_japanese { get { return ""; } }
+        public static string lp_sales_agent_thai { get { return ""; } }
+        public static string lp_sales_manager { get { return ""; } }
+        public static string lp_senior_accountant { get { return ""; } }
+        public static string lp_ui_designer { get { return ""; } }
+        public static string lp_web_designer { get { return ""; } }
+        public static string lp_web_developer { get { return ""; } }
+    }
+}
---
+++ Pluto.Selenium.Core/Selenium.Core/Process/FundamentalFields.cs
@@ -0,0 +1,13 @@
+﻿using SeleniumCore.Models;
+using System.Collections.Generic;
+
+namespace SeleniumCore.Process
+{
+    public class FundamentalFields
+    {
+        public string Name { get; set; }
+        public List<TestResultInPage> TestResultInPages { get; set; }
+        private string TestName { get; set; }
+        public static string Url { get; set; }
+    } 
+}
